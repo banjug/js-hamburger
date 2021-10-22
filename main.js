d16aa83d11b2b0ad216e1b62831133ac7120b15b
Samuele <miotto.samuele@gmail.com>
2021-10-22 16:31:37 +0200

    add correct solution

--- main.js
+++ main.js
@@ -2,19 +2,28 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerBars = document.querySelector(".header-right a i");
 const hamburgerClose = document.querySelector(".close");
 
-function checkMediaQuery() {
-    if (window.innerWidth > 1000) {
-        hamburgerMenu.style.display = 'none';
-    } else {
-        hamburgerBars.addEventListener('click', function() {
-            hamburgerMenu.style.display = 'block';
-            }
-        );    
-    hamburgerClose.addEventListener('click', function() {
-            hamburgerMenu.style.display = 'none';
-            }
-        );
-    }
-}
+// function checkMediaQuery() {
+//     if (window.innerWidth > 1000) {
+//         hamburgerMenu.style.display = 'none';
+//     } else {
+//         hamburgerBars.addEventListener('click', function() {
+//             hamburgerMenu.style.display = 'block';
+//             }
+//         );    
+//     hamburgerClose.addEventListener('click', function() {
+//             hamburgerMenu.style.display = 'none';
+//             }
+//         );
+//     }
+// }
   
- window.addEventListener('resize', checkMediaQuery);
+//  window.addEventListener('resize', checkMediaQuery);
+
+ hamburgerBars.addEventListener('click', function() {
+    hamburgerMenu.classList.add('active');
+    }
+);    
+hamburgerClose.addEventListener('click', function() {
+    hamburgerMenu.classList.remove('active');
+    }
+);
